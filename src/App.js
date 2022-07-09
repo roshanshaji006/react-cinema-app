6415231e04045effd10789a4c215c6cb3e65b37d
Roshan <rshaji@its.jnj.com>
2022-07-09 23:36:33 +0530

    circleci config

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const App = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React new
+          Learn React
         </a>
       </header>
     </div>
